4c8185f2ff0773c0a953abdfa747cd1eee3531b8
Sosuke Nakamura <so07nakamura@gmail.com>
2021-05-22 15:54:49 +0900

    ''よくわからんけどレコード関係

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_01_113932) do
+ActiveRecord::Schema.define(version: 2021_04_01_153441) do
 
   create_table "paintings", force: :cascade do |t|
     t.string "painting_name"
